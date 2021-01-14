389adea32c7422a4dce3166b16172c4100c779de
EC2 Default User <ec2-user@ip-172-31-44-111.ap-south-1.compute.internal>
2021-01-14 20:59:39 +0000

    modified the file

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello, World");
+    System.out.println("Happy Bhogi");
+    System.out.println("Happy Sankranti");
   }
  }
