821865462e5d04c065dcc6c3046368307a7fb671
EC2 Default User <ec2-user@ip-172-31-44-111.ap-south-1.compute.internal>
2021-01-16 19:38:05 +0000

    modified line for webhooks

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello, World");
     System.out.println("Happy Bhogi");
-    System.out.println("automatic trigger for webhooks worked !!")
+    System.out.println("automatic trigger for webhooks worked !!!!!!")
     System.out.println("Automation worked Congrats !!");
   }
  }
